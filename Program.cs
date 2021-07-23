86870736bf55309873c7fad4205708852f96ceec
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-07-23 15:53:07 +0530

    Pooja Added: Checking Maximum And Printing Values Using Generics

--- Program.cs
+++ Program.cs
@@ -19,15 +19,16 @@ namespace Day13_Generics
 
             int[] intArray = { 112, 344, 432, 555, 678 };
             GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
-            
+            generic.PrintMaxValue();
 
             double[] doubleArray = { 11.2, 34.4, 4.32, 55.5, 6.78 };
             GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
-            
+            genericDouble.PrintMaxValue();
 
             string[] stringArray = { "apple", "peach", "Banana", "Grapes" };
             GenericMaximum<string> genericString = new GenericMaximum<string>(stringArray);
-            
+            genericString.PrintMaxValue();
+
             Console.ReadKey();
 
 
